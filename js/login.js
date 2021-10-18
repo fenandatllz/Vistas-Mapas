482fac211a54b3fe754a66a5f3ac257fb37f7df7
Fernanda Tellez <fernandatllzc@gmail.com>
2021-10-18 10:36:00 -0700

    modal apartado

--- js/login.js
+++ js/login.js
@@ -3,6 +3,22 @@ const b = document.getElementById('registro');
 const c = document.getElementById('btn');
 const closeLogin = document.getElementById('popup-login-close');
 const login = document.querySelector('.btn-login');
+const logout = document.querySelector('#btn-logout')
+
+document.getElementById('iniciar-sesion').addEventListener("click", iniciarSesion);
+
+function iniciarSesion(){
+    sessionStorage.setItem("usuario", "prueba")
+    sessionStorage.setItem("correo", document.getElementById('correo').value)
+    sessionStorage.setItem("contra", document.getElementById('contrase_a').value)
+    sessionStorage.setItem("sesion", true)
+    alert(sessionStorage.getItem("usuario") +"\n" + sessionStorage.getItem("correo") +"\n" + sessionStorage.getItem("sesion"))
+}
+if(sessionStorage.getItem("sesion") == true)
+{
+    logout.style.display = "block";
+    login.style.display = "none";
+}
 
 function registro (){
     a.style.left ='-400px';
