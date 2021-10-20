e4ba41ebff118bb1f02326e436e91b2997c31426
Fernanda Tellez <fernandatllzc@gmail.com>
2021-10-19 18:01:49 -0700

    Zoom en planos Costa Dorada

--- js/navegacion.js
+++ js/navegacion.js
@@ -21,7 +21,21 @@ $(document).ready(function(){
         let dvEnganche = document.getElementById("mostrarEnganche");
         dvEnganche.style.display = checkEnganche.checked ? "block" : "none";
     }
-    
+//Botones Zoom
+let zoom = 1;
+		
+		$('.zoom').on('click', function(){
+			zoom += 0.1;
+			$('.target').css('transform', 'scale(' + zoom + ')');
+		});
+		$('.zoom-init').on('click', function(){
+			zoom = 1;
+			$('.target').css('transform', 'scale(' + zoom + ')');
+		});
+		$('.zoom-out').on('click', function(){
+			zoom -= 0.1;
+			$('.target').css('transform', 'scale(' + zoom + ')');
+		});
 
 // Carrusel
 if(document.querySelector('#container-slider')){
